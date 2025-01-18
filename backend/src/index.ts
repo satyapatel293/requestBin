bb0bca582cdfbc12c0b5da3e38baa51347123df9
satyapatel293 <satyapatel293@gmail.com>
2025-01-18 16:08:32 -0500

    Add PostgreSQL seed file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,6 +9,12 @@ app.get('/ping', (_req, res) => {
   res.send('pong');
 });
 
+app.get('/satya/*', (_req, res) => {
+  console.log('someone pinged here');
+  res.json(_req.headers);
+});
+
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
